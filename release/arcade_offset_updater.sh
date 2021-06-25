a8b8fe4e759ccf52b74587ee8bde2e0454468f10
atrac17 <32810066+atrac17@users.noreply.github.com>
2021-06-25 16:54:36 -0400

    Update arcade_offset_updater.sh

--- release/arcade_offset_updater.sh
+++ release/arcade_offset_updater.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+echo 
+
 echo "Arcade Offset Updater"
 
-echo -n "Grabbing latest Arcade Offset MRAs...."
+echo 
+
+echo -n "Grabbing latest Arcade Offset MRAs..."
 
 rm -rf "/media/fat/_Arcade/_Arcade Offset" &&
 
@@ -12,4 +16,8 @@ cd "/media/fat/_Arcade/_Arcade Offset" &&
 
 curl -s --insecure https://raw.githubusercontent.com/atrac17/Arcade_Offset/main/release/misterfpga_arcade_offset-20200625.txz | tar -Jxf - --no-same-owner --no-same-permissions &&
 
-echo "Complete"
+echo 
+
+echo "Update Complete"
+
+exit 0

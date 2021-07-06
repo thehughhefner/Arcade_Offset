5e5be1348047c5b6c2506ed184c67afab8896420
atrac17 <32810066+atrac17@users.noreply.github.com>
2021-07-06 02:47:38 -0400

    Release 20210706
    -updated arcade_offset_update/updater.sh

--- release/arcade_offset_updater.sh
+++ release/arcade_offset_updater.sh
@@ -5,7 +5,7 @@ RED='\033[0;31m'
 PRP='\033[0;35m'
 NC='\033[0m'
 
-echo 
+echo -e ${NC}
 echo -e "${PRP}Arcade Offset ${NC}is updating..."
 echo 
 echo -e -n "Grabbing latest ${PRP}Arcade Offset ${NC}MRA files..."
@@ -16,6 +16,6 @@ cd "/media/fat/_Arcade/_Arcade Offset" &&
 curl -s --cacert /etc/ssl/certs/cacert.pem https://raw.githubusercontent.com/atrac17/Arcade_Offset/main/release/misterfpga_arcade_offset-20210706.txz | tar -Jxf - --no-same-owner --no-same-permissions &&
 echo 
 echo -e "${PRP}Update Complete"
-echo 
+echo -e ${NC}
 
 exit 0

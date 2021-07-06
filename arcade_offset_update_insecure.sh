91f5cd4f9bc4cfd193d4f90d0c5d6e3b53f5ba6c
atrac17 <32810066+atrac17@users.noreply.github.com>
2021-07-06 07:16:20 -0400

    Added insecure version
    Apparently, users who have run the security_fixes.sh have issues downloading with `curl -s --cacert /etc/ssl/certs/cacert.pem`.
    
    I am not going to trouble shoot this script from mister-devel. I've added an option if the script will not run `--cacert` to use `--insecure` from a different script..

---
+++ arcade_offset_update_insecure.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#written by atrac17
+
+RED='\033[0;31m'
+PRP='\033[0;35m'
+NC='\033[0m'
+
+echo -e ${NC} 
+echo -e "${PRP}             __    _                      _          ___   __  __          _"
+echo -e "${PRP}            / /   / \   _ __ ___ __ _  __| | ___    / _ \ / _|/ _|___  ___| |_"
+echo -e "${PRP}           / /   / _ \ |  __/ __/ _  |/ _  |/ _ \  | | | | |_| |_/ __|/ _ \ __|"
+echo -e "${PRP}        _ / /   / ___ \| | | (_| (_| | (_| |  __/  | |_| |  _|  _\__ \  __/ |_"
+echo -e "${PRP}       (_)_/___/_/   \_\_|  \___\__,_|\__,_|\___|   \___/|_| |_| |___/\___|\__|"
+echo -e "${PRP}          |_____|"
+echo 
+echo -e "${RED}** WARNING: ** ${NC}This script will ${RED}erase ${NC}and install the ${RED}/_Arcade Offset directory${NC} again.
+${NC}Ensure to back up your mra files you've ${RED}modified ${NC}in this ${RED}directory path ${NC}before running."
+echo 
+echo -e "${NC}Press ctrl-c to exit or any key to continue..."
+read -n 1
+curl -s --insecure --fail --location https://raw.githubusercontent.com/atrac17/Arcade_Offset/main/release/arcade_offset_updater_insecure.sh | bash -
+
+exit 0
---
+++ release/arcade_offset_updater_insecure.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#written by atrac17
+
+RED='\033[0;31m'
+PRP='\033[0;35m'
+NC='\033[0m'
+
+echo -e ${NC}
+echo -e "${PRP}Arcade Offset ${NC}is updating..."
+echo 
+echo -e -n "Grabbing latest ${PRP}Arcade Offset ${NC}MRA files..."
+echo 
+rm -rf "/media/fat/_Arcade/_Arcade Offset" &&
+mkdir -p "/media/fat/_Arcade/_Arcade Offset" &&
+cd "/media/fat/_Arcade/_Arcade Offset" &&
+curl -s --insecure https://raw.githubusercontent.com/atrac17/Arcade_Offset/main/release/misterfpga_arcade_offset-20210706.txz | tar -Jxf - --no-same-owner --no-same-permissions &&
+echo 
+echo -e "${PRP}Update Complete"
+echo -e ${NC}
+
+exit 0
